e2c8f4294778d42856185ca03bd2e33797cb2b88
Gazi Akter <gaziakterdev@gmail.com>
2024-07-26 21:55:21 +0600

    plugin init

---
+++ wp-mega.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: WP Mega
+* Plugin URI: http://gaziakter.com/plugins/wp-mega/
+* Author URI: http://gaziakter.com/
+* Description: WO Mega only for practical purpose. 
+* Author: Gazi Akter
+* Version: 1.0.0
+ */
