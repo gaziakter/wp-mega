68f0bc3d3c54f9e3922149c3dca0e956c1150783
Gazi Akter <gaziakterdev@gmail.com>
2024-07-27 22:43:49 +0600

    add new address form

--- includes/Admin/views/address-new.php
+++ includes/Admin/views/address-new.php
@@ -1,3 +1,35 @@
 <div class="wrap">
     <h1 class="wp-heading-inline"><?php _e('New Address', 'wp-mega'); ?></h1>
+
+    <form action="" method="post">
+        <table class="form-table">
+            <tbody>
+                <tr>
+                    <th class="row">
+                        <label for="name"><?php _e('Name', 'wp-mega'); ?></label>
+                    </th>
+                    <td>
+                        <input type="text" name="name" id="name" class="regular-text" value="">
+                    </td>
+                </tr>
+                <tr>
+                    <th class="row">
+                        <label for="address"><?php _e('Address', 'wp-mega'); ?></label>
+                    </th>
+                    <td>
+                        <textarea name="address" id="address" class="regular-text"></textarea>
+                    </td>
+                </tr>
+                <tr>
+                    <th class="row">
+                        <label for="phone"><?php _e('Phone', 'wp-mega'); ?></label>
+                    </th>
+                    <td>
+                        <input type="text" name="phone" id="phone" class="regular-text" value="">
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <?php submit_button( __('Add Address', 'wp-mega'), 'primary', 'submit_address', true, null ); ?>
+    </form>
 </div>
