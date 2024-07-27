1f15c0830d2ff9e4d6df116fc792ee55b21e64f9
Gazi Akter <gaziakterdev@gmail.com>
2024-07-27 19:52:41 +0600

    address book sub menu

--- includes/Admin/Menu.php
+++ includes/Admin/Menu.php
@@ -8,17 +8,34 @@ class Menu{
     }
 
     public function admin_menu(){
+
+        $parent_slug =  'wp-mega';
+        $capability = 'manage_options';
+
         add_menu_page(
             __('WP Mega', 'wp-mega'),
             __('WP Mega', 'wp-mega'),
-            'manage_options',
-            'wp-mega',
+            $capability,
+            $parent_slug,
             [$this, 'plugin_page'],
             'dashicons-cover-image'
+        );
+
+        add_submenu_page( 
+            $parent_slug,
+            __('Address Book', 'wp-mega'), 
+            __('Address Book', 'wp-mega'),
+            $capability, 
+            'wp-mega-addressbook', 
+            [$this, 'addressbook_page'],
         );
     }
 
     public function plugin_page(){
         echo "Hello Bangladesh!";
+    }
+
+    public function addressbook_page(){
+        echo 'Adress book dashbord';
     }
 }
