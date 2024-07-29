e8b3229ad2a262355dc22b8189ddac626162e703
Gazi Akter <gaziakterdev@gmail.com>
2024-07-29 06:50:40 +0600

    Create table

--- includes/Installer.php
+++ includes/Installer.php
@@ -38,15 +38,20 @@ class Installer{
         $charset_collate = $wpdb->get_charset_collate();
 
         $schema = "CREATE TABLE IF NOT EXISTS `{$wpdb->prefix}ac_addresses` (
-    `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
-    `name` varchar(100) NOT NULL,
-    `address` varchar(255) DEFAULT NULL,
-    `phone` varchar(30) DEFAULT NULL,
-    `created_by` bigint(20) unsigned NOT NULL,
-    `created_at` datetime NOT NULL,
-    KEY `id` (`id`)
-    )  $charset_collate;
-    ";
+        `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
+        `name` varchar(100) NOT NULL,
+        `address` varchar(255) DEFAULT NULL,
+        `phone` varchar(30) DEFAULT NULL,
+        `created_by` bigint(20) unsigned NOT NULL,
+        `created_at` datetime NOT NULL,
+        KEY `id` (`id`)
+        )  $charset_collate;
+        ";
 
+        if(! function_exists('dbDelta')){
+            require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+        }
+
+        dbDelta( $schema );
     }
 }
