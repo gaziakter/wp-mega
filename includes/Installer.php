fd4d28de4125c4835a0a79b4452a280908c77229
Gazi Akter <gaziakterdev@gmail.com>
2024-07-29 06:11:31 +0600

    schena create

--- includes/Installer.php
+++ includes/Installer.php
@@ -33,5 +33,20 @@ class Installer{
      */
     public function create_table(){
 
+        global $wpdb;
+
+        $charset_collate = $wpdb->get_charset_collate();
+
+        $schema = "CREATE TABLE IF NOT EXISTS `{$wpdb->prefix}ac_addresses` (
+    `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
+    `name` varchar(100) NOT NULL,
+    `address` varchar(255) DEFAULT NULL,
+    `phone` varchar(30) DEFAULT NULL,
+    `created_by` bigint(20) unsigned NOT NULL,
+    `created_at` datetime NOT NULL,
+    KEY `id` (`id`)
+    )  $charset_collate;
+    ";
+
     }
 }
