1672bea3c6f9799e97bbb4d500b34647bf4119d3
Gazi Akter <gaziakterdev@gmail.com>
2024-08-02 08:55:27 +0600

    get address

--- includes/functions.php
+++ includes/functions.php
@@ -33,7 +33,37 @@ function wp_mega_insert_address($args = []){
     if(! $inserted){
         return new \WP_Error('failed-to-insert', __('Failed to insert data', 'wp-mega'));
     }
-
     return $wpdb->insert_id;
+}
 
+
+/**
+ * 
+ */
+function wp_mega_get_addressess($args = []){
+    global $wpdb;
+
+    $defaults = [
+        'number' => 20,
+        'offset' => 0,
+        'orderby' => 'id',
+        'order' => 'ASC'
+    ];
+
+    $args = wp_parse_args( $args, $defaults );
+
+    $items = $wpdb->get_results(
+        $wpdb->prepare(
+            "SELECT * FROM {$wpdb->prefix}ac_addresses
+            ORDER BY %s %s
+            LIMIT %d, %d",
+
+            $args['orderby'],
+            $args['orderby'],
+            $args['offset'],
+            $args['number']
+        )
+    );
+
+    return  $items;
 }
